44efe7b3786a26133c4df65c2aa80951b2385922
Min <42695789+miniii222@users.noreply.github.com>
2018-12-10 15:17:57 +0900

    Add files via upload
    useful preprocessing code in r

---
+++ preprocessing/useful_code.r
@@ -0,0 +1,14 @@
+﻿#1.column na 한번에 보여주기
+library(dplyr)
+
+df %>% select(everything()) %>%  # select every columns
+
+summarise_all(funs(sum(is.na(.))))
+
+colSums(is.na(df))
+
+#2.변수 이름 그대로 적용하기
+##ex
+a=5
+print(a) #5출력
+print(deparse(substitute(a)) #a출력
